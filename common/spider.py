859cea1f001a0ef0dea65808f3c3b040b045ad42
songjinlong <jinlong.song@flightroutes24.com>
2022-11-10 15:09:22 +0800

    update

--- common/spider.py
+++ common/spider.py
@@ -1,19 +1,76 @@
+import os
+import time
 import requests
 from common import useragent
 from common import connect
 import traceback
 import execjs
+import logging
+import datetime
 
 class Spider(object):
-
-    def __init__(self):
+    def __init__(self,spider_name):
         self.session = requests.Session()
-        self.post = self.session.post
-        self.get = self.session.get
         self.user_agent = useragent.get_user_agent()
+        self.logger = self.save_logger(spider_name)
 
     def connent(self):
         return connect
+
+    # get请求
+    def get(self,url = None,headers = None,params = None,verify = True,timeout = 10,retry_times = 3,proxies=None,**kwargs):
+        response = None
+        # 定义重试次数
+        for i in range(retry_times):
+            try:
+                response = self.session.get(url=url,headers=headers,params=params,verify=verify,timeout=timeout,
+                                             **kwargs)
+                if response.status_code == 500:
+                    self.logger.info('服务器遇到错误, 无法完成请求, status_code:{}'.format(response.status_code))
+                    break
+                if response.status_code == 503:
+                    self.logger.info('服务器不可用, 服务器超载或已停止使用, status_code:{}'.format(response.status_code))
+                    break
+                if response.status_code == 504:
+                    self.logger.info('服务器网关或代理超时, status_code:{}'.format(response.status_code))
+                    continue
+                if response.status_code == 404:
+                    self.logger.info('服务器找不到该网页, status_code:{}, url:{}'.format(response.status_code,url))
+                    break
+                if response.status_code == 200:
+                    self.logger.info(url + ' : ' + str(response.status_code))
+                    return response
+            except:
+                self.logger.info('请求失败, url:{}, error is:{}'.format(url,traceback.format_exc()))
+                continue
+        return None
+
+    # post请求
+    def post(self,url = None,headers = None,params = None,verify = True,timeout = 5,retry_times = 3,proxies=None,**kwargs):
+        response = None
+        for i in range(retry_times):
+            try:
+                response = self.session.post(url=url, headers=headers, params=params, verify=verify, timeout=timeout,
+                                             **kwargs)
+                if response.status_code == 500:
+                    self.logger.info('服务器遇到错误, 无法完成请求, status_code:{}'.format(response.status_code))
+                    break
+                if response.status_code == 503:
+                    self.logger.info('服务器不可用, 服务器超载或已停止使用, status_code:{}'.format(response.status_code))
+                    break
+                if response.status_code == 504:
+                    self.logger.info('服务器网关或代理超时, status_code:{}'.format(response.status_code))
+                    continue
+                if response.status_code == 404:
+                    self.logger.info('服务器找不到该网页, status_code:{}, url:{}'.format(response.status_code, url))
+                    break
+                if response.status_code == 200:
+                    self.logger.info(url + ' : ' + str(response.status_code))
+                    return response
+            except:
+                self.logger.info('请求失败, url:{}, error is:{}'.format(url, traceback.format_exc()))
+                continue
+        return None
 
     # 调用js函数
     def run_js(self,js_name,func_name):
@@ -26,3 +83,26 @@ class Spider(object):
         except:
             print(traceback.format_exc())
             return None
+
+    # 保存日志
+    def save_logger(self,spider_name):
+        logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # 创建logger对象
+        logger = logging.getLogger('')
+        logger.handlers.clear()
+        # 现在的时间
+        now = datetime.date.today()
+        # 日志地址
+        log_path = '../logs/{}'.format(now)
+        # 创建文件
+        if not os.path.exists('../logs'):
+            os.mkdir('../logs')
+        if not os.path.exists(log_path):
+            os.mkdir(log_path)
+
+        handler = logging.FileHandler(
+            "../logs/{}/spider_{}_{}.log".format(now, spider_name, now),encoding='utf-8')
+        logger.addHandler(handler)
+        # 定义日志输出格式
+        handler.setFormatter(logging.Formatter('[%(asctime)s] %(module)s - %(lineno)d/  %(message)s'))
+        return logger
--- spider_code/ggzy.py
+++ spider_code/ggzy.py
@@ -1,13 +1,13 @@
 from lxml import etree
 from common.spider import Spider
+import traceback
 import execjs
-
 
 class Ggzy(Spider):
     def __init__(self):
-        super().__init__()
+        super().__init__('ggzy')
         self.referer = ''
-        self.msg = '天津市公共资源交易平台抓取'
+        self.msg = '天津市公共资源交易平台'
 
     # 获取首页及所有翻页的页面连接
     def get_index(self):
@@ -44,29 +44,97 @@ class Ggzy(Spider):
             'User-Agent': self.user_agent,
             'Referer':self.referer
         }
+
         # 获取分页url
         for url in page_urls:
             response = self.get(url=url,headers=headers)
-            tree = etree.HTML(response.text)
-            a_url_lists = tree.xpath('//li/div[@class="article-list3-t"]/a/@url')
-            times = tree.xpath('//li/div[@class="article-list3-t"]/div[@class="list-times"]/text()')
-            for i in range(len(tree.xpath('//div[@class="article-list3-t"]'))):
-                fucnname = 'get_aes("{}")'.format(a_url_lists[i])
-                js_code = self.run_js('ggzy.js',fucnname)
-                page_dict = {
-                    'url':js_code,
-                    'referer':a_url_lists[i],
-                    'title':'',
-                    'datetime': times[i].strip()
-                }
+            if response.status_code == 200:
+                try:
+                    messages = []
+                    tree = etree.HTML(response.text)
+                    a_url_lists = tree.xpath('//li/div[@class="article-list3-t"]/a/@url')
+                    times = tree.xpath('//li/div[@class="article-list3-t"]/div[@class="list-times"]/text()')
+                    for i in range(len(tree.xpath('//div[@class="article-list3-t"]'))):
+                        fucnname = 'get_aes("{}")'.format(a_url_lists[i])
+                        js_code = self.run_js('ggzy.js',fucnname)
+                        page_dict = {
+                            'url':js_code,
+                            'referer':url,
+                            'title':'',
+                            'datetime': times[i].strip(),
+                        }
+                        page_dict = self.get_text_page(page_dict)
+                        messages.append(page_dict)
+                except:
+                    self.logger.info('解析分页url出错, error is:{}'.format(traceback.format_exc()))
+                    self.msg = '解析分页url出错'
+                    return messages
+            else:
+                self.logger.info('URL:{}, 请求失败'.format(url))
+                self.msg = '分页url请求失败，请查看日志'
+                return None
+        return messages
+    # 获取文章页面的内容
+    def get_text_page(self,page_dict):
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Referer': page_dict['referer'],
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': self.user_agent,
+        }
+        response = self.get(url=page_dict['url'],headers=headers,verify=False)
+        if response.status_code:
+            try:
+                tree = etree.HTML(response.text)
+                # 标题
+                title = tree.xpath('//div[@class="content-title"]/text()')[0]
+                page_dict['title'] = title.strip()
+                # 浏览次数
+                views = 0
+                if tree.xpath('//span[@id="views"]/text()'):
+                    views = tree.xpath('//span[@id="views"]/text()')[0].strip()
+                page_dict['views'] = int(views)
+                # 信息来源
+                origin = tree.xpath('//a[@class="originUrl"]/text()')[0].strip()
+                page_dict['origin'] = origin
+                # 页面文字
+                contents = tree.xpath('//div[@id="content"]/p/span/text()')
+                page_text = ''
+                for content in contents:
+                    page_text += content.replace('\xa0','') + '\r'
+                page_dict['page_text'] = page_text
+                return page_dict
+            except:
+                self.logger.info('文章页面解析失败, error is:{}'.format(traceback.format_exc()))
+                self.msg = '文章页面解析失败'
+                return None
+        else:
+            self.logger.info('文章页面请求失败')
+            self.msg = '文章页面请求失败'
+            return None
 
-
+    # 主函数
     def handel(self):
+        self.logger.info('天津市公共资源交易平台开始抓取')
         page_urls = self.get_index()
         message = self.get_page(page_urls)
-        print(self.msg)
+        if message:
+            alldata = {
+                'msg': self.msg,
+                'message': message
+            }
+        else:
+            alldata = {
+                'msg':self.msg,
+                'message':[]
+            }
+        self.logger.info('天津市公共资源交易平台结束抓取')
+        return alldata
 
 if __name__ == "__main__":
     ggzy = Ggzy()
-    outpute = ggzy.handel()
-    print(outpute)
+    alldata = ggzy.handel()
+    print(alldata)
